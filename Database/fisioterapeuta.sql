5633612ce763d5b032a3b90f330d359e42480957
Luis Vasquez <vluis5457@gmail.com>
2022-06-28 09:43:49 -0600

    cambiando db

--- Database/fisioterapeuta.sql
+++ Database/fisioterapeuta.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-06-2022 a las 01:29:32
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 28-06-2022 a las 17:42:57
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `fisioterapeta`
+-- Base de datos: `fisioterapeuta`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,7 @@ CREATE TABLE `category` (
   `categoria` varchar(250) NOT NULL,
   `descripcion` text NOT NULL,
   `imagen` blob NOT NULL,
-  `color_heda_tittle` varchar(7) NOT NULL
+  `color_head_tittle` varchar(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -43,12 +43,32 @@ CREATE TABLE `category` (
 
 CREATE TABLE `exercices` (
   `id_exercice` int(11) NOT NULL,
-  `Exercice_name` varchar(100) NOT NULL,
+  `exercice_name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `img_path` varchar(255) NOT NULL,
   `pdf_path` varchar(255) NOT NULL,
   `id_category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reset_code` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `password`, `reset_code`) VALUES
+(1, 'luis23032002', 'ee5dcad511e40089cacf3bb36d8c92e6', '5sadas5');
 
 --
 -- Índices para tablas volcadas
@@ -66,6 +86,34 @@ ALTER TABLE `category`
 ALTER TABLE `exercices`
   ADD PRIMARY KEY (`id_exercice`),
   ADD KEY `id_category` (`id_category`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `exercices`
+--
+ALTER TABLE `exercices`
+  MODIFY `id_exercice` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
