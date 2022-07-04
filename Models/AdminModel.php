b5ba88c524df5a2384f90c3b7ce8a075e3b78387
Juan Gomez <acunajuan429@gmail.com>
2022-07-04 14:44:48 -0600

    Crud agregado

--- Models/AdminModel.php
+++ Models/AdminModel.php
@@ -26,4 +26,66 @@ class AdminModel extends Model{
 
        return $resultado;
     }
+    /*Crud categorias*/ 
+
+    public function addCategory(){
+        $sql = "INSERT INTO category VALUES(?,'?','?','?','?');";
+        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
+  
+    public function dropCategory(){
+        $sql = "DELETE FROM category WHERE id_category = ?;";
+        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
+    public function editCategory(){
+        $sql = "UPDATE 
+        category SET categoria = '?', descripcion = '?',imagen ='?', color_head_tittle='?'
+        WHERE id_category = ?;";        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
+
+    /*Crud categorias*/ 
+
+    public function addExercice(){
+        $sql = "INSERT INTO exercices VALUES(?,'?','?','?','?',?);";
+        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
+    public function dropExercice(){
+        $sql = "DELETE FROM exercices WHERE id_exercice = ?;";
+        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
+    public function editExercice(){
+        $sql = "UPDATE 
+        exercices SET exercice_name = '?',description = '?',img_path = '?',pdf_path = '?',id_category= '?'
+        WHERE id_exercice = ?;";        
+        $stm = $this->conexion->prepare($sql);
+        $stm->execute();
+        $resultado = $stm->fetchAll();
+
+       return $resultado;
+    }
 }
